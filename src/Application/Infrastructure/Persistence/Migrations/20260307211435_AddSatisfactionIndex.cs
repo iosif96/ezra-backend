bb74f6bae333a7530057f2f5be58cb73f2e1aa0a
Iosif Chesca <iosif.chesca@gmail.com>
2026-03-07 23:14:53 +0200

    Migration

---
+++ src/Application/Infrastructure/Persistence/Migrations/20260307211435_AddSatisfactionIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSatisfactionIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "SatisfactionIndex",
+                table: "Messages",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SatisfactionIndex",
+                table: "Messages");
+        }
+    }
+}
